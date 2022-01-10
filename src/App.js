e4889d533ff4fec3f9f50ac64eeca83e4f74923e
Becky Fox <foxy2530@gmail.com>
2022-01-10 17:09:32 +0000

    added the header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
-// import Header from './Components/Header';
+import Header from './Components/Header';
 import Home from './Components/Home';
-import Section from './Components/Section';
+// import Section from './Components/Section';
+
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Home />
 
     </div>
